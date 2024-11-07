305a5f87c983474f552be294db735ddf12f91fc8
saramadonia <146438182+saramadonia@users.noreply.github.com>
2024-11-07 16:24:58 +0100

    ciao amo

---
+++ test/test_uml_handler.py
@@ -0,0 +1,8 @@
+import unittest 
+from uml_classes import IdentifiableEntity
+
+class TestIdentifiableEntity(unittest.TestCase):
+    def test_getId(self):
+        identifiable_entity = IdentifiableEntity('orchid-0000-1234-9363-2826')
+        identifier = identifiable_entity.getId()
+        self.assertEqual(identifier, 'orchid-0000-1234-9363-2826')
